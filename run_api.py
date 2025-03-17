4b2add6c9400898a2c58780302f02e524e57db39
Kevin Xiao <kevin81xiao@gmail.com>
2025-03-17 23:40:20 +0900

    Add --realmoney flag to run_api.py to support real money trading configuration

--- run_api.py
+++ run_api.py
@@ -6,6 +6,7 @@ Script to start the Auto-Trader API server using gunicorn
 
 import os
 import sys
+import argparse
 from dotenv import load_dotenv
 from core.logging_config import get_logger
 
@@ -20,6 +21,20 @@ def main():
     Start the API server using gunicorn
     """
     try:
+        # Parse command line arguments
+        parser = argparse.ArgumentParser(description='Start the Auto-Trader API server')
+        parser.add_argument('--realmoney', action='store_true', 
+                           help='Use real money trading configuration instead of paper trading')
+        args = parser.parse_args()
+        
+        # Set environment variable for connection config based on the flag
+        if args.realmoney:
+            os.environ['CONNECTION_CONFIG'] = 'connection_real.json'
+            logger.warning("Using REAL MONEY trading configuration! Be careful with your orders!")
+        else:
+            os.environ['CONNECTION_CONFIG'] = 'connection.json'
+            logger.info("Using paper trading configuration")
+        
         # Get port from environment variable or use default
         port = os.environ.get('PORT', '5000')
         workers = os.environ.get('WORKERS', '4')
